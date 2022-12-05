757c40156b54a88dbf73e188cf71664a97b5f26f
Joy Valdez <43526824+jvaldez90@users.noreply.github.com>
2022-12-05 13:30:23 -0500

    Update SOEN287.sql

--- SOEN287.sql
+++ SOEN287.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Dec 02, 2022 at 05:16 PM
+-- Generation Time: Dec 05, 2022 at 07:29 PM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.1.6
 
@@ -29,31 +29,32 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `grade_records` (
   `id` int(2) NOT NULL,
-  `Name` varchar(50) NOT NULL,
-  `Assignment 1` float DEFAULT NULL,
-  `Assignment 2` float DEFAULT NULL,
-  `Assignment 3` float DEFAULT NULL,
-  `Midterm` float DEFAULT NULL,
-  `Final Exam` float DEFAULT NULL,
-  `Average` int(11) DEFAULT NULL,
-  `Final Grade` varchar(2) DEFAULT NULL
+  `name` varchar(50) NOT NULL,
+  `assignment_1` float DEFAULT NULL,
+  `assignment_2` float DEFAULT NULL,
+  `assignment_3` float DEFAULT NULL,
+  `midterm` float DEFAULT NULL,
+  `final_exam` float DEFAULT NULL,
+  `average` int(11) DEFAULT NULL,
+  `final_grade` varchar(2) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `grade_records`
 --
 
-INSERT INTO `grade_records` (`id`, `Name`, `Assignment 1`, `Assignment 2`, `Assignment 3`, `Midterm`, `Final Exam`, `Average`, `Final Grade`) VALUES
+INSERT INTO `grade_records` (`id`, `name`, `assignment_1`, `assignment_2`, `assignment_3`, `midterm`, `final_exam`, `average`, `final_grade`) VALUES
 (1, 'Barry Allen', 75, 80, 63, 67, 73, NULL, NULL),
 (2, 'Oliver Queen', 65, 74, 82, 63, 87, NULL, NULL),
 (3, 'Kara Danvers', 62, 73, 88, 92, 78, NULL, NULL),
-(4, 'Jerfferson Pierce', 65, 88, 95, 73, 61, NULL, NULL),
+(4, 'Jefferson Pierce', 65, 88, 95, 73, 61, NULL, NULL),
 (5, 'Bruce Wayne', 77, 80, 96, 95, 66, NULL, NULL),
 (6, 'Lena Luther', 78, 82, 72, 64, 75, NULL, NULL),
 (7, 'Felicity Smoak', 98, 88, 78, 72, 95, NULL, NULL),
 (8, 'John Diggle', 67, 73, 65, 59, 60, NULL, NULL),
 (9, 'Caitlin Snow', 67, 76, 88, 55, 87, NULL, NULL),
-(10, 'Cisco Ramon', 77, 72, 75, 79, 71, NULL, NULL);
+(10, 'Cisco Ramon', 77, 72, 75, 79, 71, NULL, NULL),
+(11, 'Clark Kent', 51, 52, 53, 54, 55, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -123,7 +124,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `grade_records`
 --
 ALTER TABLE `grade_records`
-  MODIFY `id` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `users`
